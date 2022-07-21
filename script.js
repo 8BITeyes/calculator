0dd00f42295515477662b45148e018b24f149823
RyanFrazin <RyanFrazin@Gmail.com>
2022-07-20 19:02:27 -0700

    Ryan Frazin Jul 20, 2022 19:02
    Format CSS to be shorter and include all buttons
    
    Add id to multiple buttons in HTML
    
    Add display functions and number functions in JS.

--- script.js
+++ script.js
@@ -16,14 +16,16 @@ function operate(operator, a, b){
     }
 }
 
+//adding buttons to calculator container
 let numberContainer = document.querySelector('.numbercontainer');
 let bottomContainer = document.querySelector('.bottomcontainer');
 
-for(let i = 1; i <= 9; i++) { //adds buttons to html for each number 1 - 9 (makes HTML neater)
+for(let i = 1; i <= 9; i++) { //adds buttons to html for each number 1 - 9
     let button = document.createElement('button');
     numberContainer.appendChild(button);
     button.textContent = `${i}`;
     button.classList.add('button');
+    button.value = button.textContent;
 }
 
 let zero = document.createElement('button'); //adds 0 button (longer properties)
@@ -35,3 +37,36 @@ let dot = document.createElement('button'); //adds "." button
 bottomContainer.appendChild(dot);
 dot.textContent = ".";
 dot.classList.add('button');
+dot.value = ".";
+
+//display functions
+const display = document.querySelector('.display');
+let displayValue = '';
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(function(currentBtn) { //display changes based on text content of *number* button pressed
+    currentBtn.addEventListener('click', () => {
+        displayValue += `${currentBtn.value}`;
+        display.textContent += currentBtn.textContent;
+    })
+});
+
+//numbers and operations
+let numbers = []; //holds all inputted numbers
+let ops = []; //holds all inputted operators
+
+function currentNum() {
+    let num = displayValue;
+    numbers.push(num);
+    displayValue = '';
+    return num;
+}
+
+const operators = document.querySelectorAll('#operator');
+operators.forEach(function(currentOp) {
+    currentOp.addEventListener('click', () => {
+        display.textContent += currentOp.textContent;
+        ops.push(currentOp.textContent);
+        currentNum();
+    })
+});
